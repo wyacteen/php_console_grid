0077f8ba020e96dc64f11f2b8a8fcb322b466ea3
Wisam Yacteen <wyacteen@hotmail.com>
2017-07-30 20:14:47 -0400

    Added support for top_picture api and search tweaks

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+// This is the api route that will be called by console grid.
+Route::get('/top_picture/', 'GameController@topPictureSearch');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
